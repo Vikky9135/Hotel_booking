9d49822343c9dcd923582e75de284862819d7702
Vikram <vikramvikky2003@gmail.com>
2023-08-01 00:48:06 +0530

    Develop Main loop and Hotel class.

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
-class User:
-    def view_hotels(self):
-        pass
+import pandas
+
+df = pandas.read_csv("hotels.csv", dtype={"id": str})
 
 
 class Hotel:
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
+
     def book(self):
+        """Book a hotel by changing its availability to no"""
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
+
+    def available(self):
+        """Check's the hotel is available"""
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
+
+
+class ReservationTicket:
+    def __init__(self, customer_name, hotel_object):
         pass
 
-
-class Reservation:
     def generate(self):
         pass
+
+
+# Main loop
+print(df)
+hotel_ID = input("Enter the id of the hotel: ")
+hotel = Hotel(hotel_ID)
+
+if hotel.available():
+    hotel.book()
+    name = input("Enter your name: ")
+    reservation_ticket = ReservationTicket(name, hotel)
+    print(reservation_ticket.generate())
+else:
+    print("Hotel not found!")
