61832275b8008121b0896ccc73c6727b1a05495d
Vikram <vikramvikky2003@gmail.com>
2023-08-01 14:04:55 +0530

    Inheritance - Added credit card authentication.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas
 
 df = pandas.read_csv("hotels.csv", dtype={"id": str})
 df_cards = pandas.read_csv("cards.csv", dtype=str).to_dict(orient="records")
+df_card_security = pandas.read_csv("card_security.csv", dtype=str)
 
 
 class Hotel:
@@ -51,18 +52,31 @@ class CreditCard:
         else:
             return False
 
+
+class SecurityCreditCard(CreditCard):
+    def authenticate(self, given_password):
+        password = df_card_security.loc[df_card_security["number"] == self.number, "password"].squeeze()
+        if password == given_password:
+            return True
+        else:
+            return False
+
 # Main loop
 print(df)
 hotel_ID = input("Enter the id of the hotel: ")
 hotel = Hotel(hotel_ID)
 
 if hotel.available():
-    credit_card = CreditCard(number="1234567890123456")
+    credit_card = SecurityCreditCard(number="1234567890123456")
     if credit_card.validate(expiration="12/23", holder="Vikram", cvc="321"):
-
-        hotel.book()
-        name = input("Enter your name: ")
-        reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
-        print(reservation_ticket.generate())
+        if credit_card.authenticate(given_password="mypass"):
+            hotel.book()
+            name = input("Enter your name: ")
+            reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+            print(reservation_ticket.generate())
+        else:
+            print("Credit card authentication Failed")
+    else:
+        print("There is a problem with your payment")
 else:
     print("Hotel is Full !")
